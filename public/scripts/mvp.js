f518baac9e6188963e8fc7a99d55d8e06aa74890
CHRISTOPHER SPENCER BUJA <csbuja@umich.edu>
2014-11-09 05:37:23 -0500

    changed wording of second page

--- public/scripts/mvp.js
+++ public/scripts/mvp.js
@@ -4,8 +4,7 @@ $(document).ready(function(){
 		var title = $('#title');
 		title.children().remove();
 		title.append('<span style="font-size: 40px;" class="titleSpan"> Bar O\' Clock is still in the works.</span></br>' +
-		' <span style="font-size: 40px;" class="titleSpan">We\'ll let you when it\'s ready!</span></br>'+
-		' <span style="font-size: 40px;" class="titleSpan">Let us know what your email is, so we can contact you when it is.</span>');
+		' <span style="font-size: 40px;" class="titleSpan">Let us know what your email is. We\'ll contact you when the app is ready.</span>');
 		title.append('</br>');
 		title.append('</br>');
 		title.append('<iframe style="height: 300px;" src="https://docs.google.com/forms/d/1r18FEAY9a6Mu0CLwa4sRipcBmk9VsDkUOPSJwoGUrp0/viewform?embedded=true" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>');
