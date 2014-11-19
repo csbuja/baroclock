144c390e6d5970fe13132adef4c01de04847cc08
CHRISTOPHER SPENCER BUJA <csbuja@umich.edu>
2014-11-19 14:36:08 -0500

    added a back home functionality to the web page

--- public/scripts/mvp.js
+++ public/scripts/mvp.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
 		$('.mission').remove();
 		var title = $('#title');
 		title.children().remove();
+		$('#title').append('<span style="position: absolute; top: 0; left: 0;" id="backhome" > Home Page </span>');
+		$('#backhome').click(function(){location.reload();});
 		title.append('<span style="font-size: 40px;" class="titleSpan"> Bar O\' Clock is still in the works.</span></br>' +
 		' <span style="font-size: 40px;" class="titleSpan">Let us know what your email is. We\'ll contact you when the app is ready.</span>');
 		title.append('</br>');
